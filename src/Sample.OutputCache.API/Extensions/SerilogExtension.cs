236e6ddc0bc729d8899a8e61b998709559ee92d2
Ricardo Alves - Ext <ricardo.alves.ext@brf.com>
2022-12-29 14:17:00 -0300

    Add project files.

---
+++ src/Sample.OutputCache.API/Extensions/SerilogExtension.cs
@@ -0,0 +1,26 @@
+﻿using Serilog;
+using Serilog.Events;
+using Serilog.Exceptions;
+using Serilog.Filters;
+
+namespace Sample.OutputCache.API.Extensions;
+
+public static class SerilogExtension
+{
+    public static WebApplicationBuilder AddSerilog(this WebApplicationBuilder builder)
+    {
+        Log.Logger = new LoggerConfiguration()
+            .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Information)
+            .Enrich.FromLogContext()
+            .Enrich.WithExceptionDetails()
+            .Enrich.WithProperty("ApplicationName", "API Output Caching")
+            .Filter.ByExcluding(Matching.FromSource("Microsoft.AspNetCore.Hosting.Diagnostics"))
+            .WriteTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj} {Properties:j}{NewLine}{Exception}")
+            .CreateLogger();
+
+        builder.Logging.ClearProviders();
+        builder.Host.UseSerilog(Log.Logger, true);
+
+        return builder;
+    }
+}
